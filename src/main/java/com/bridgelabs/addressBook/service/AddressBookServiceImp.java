c99a80ab2be9767e2457595143cc55499904137c
hariprasad154 <hariprasas154@gmail.com>
2023-04-18 16:51:32 +0530

    refactored

--- src/main/java/com/bridgelabs/addressBook/service/AddressBookServiceImp.java
+++ src/main/java/com/bridgelabs/addressBook/service/AddressBookServiceImp.java
@@ -31,21 +31,32 @@ public class AddressBookServiceImp implements AddressBookService{
     }
 
     @Override
-    public AddressBookData getEmployeeById(int id) {
+    public AddressBookData getById(int id) {
         return addressbookRepository.findById(id).orElseThrow(() -> new CustomException(" Employee Not found .. wih id: "+ id));
     }
 
     @Override
     public AddressBookData UpdateEmployee(int id, AddressBookDto addressBookDto) {
-        AddressBookData addressBookData =this.getEmployeeById(id);
+        AddressBookData addressBookData =this.getById(id);
         addressBookData.updateData(addressBookDto);
         return addressbookRepository.save(addressBookData);
     }
 
     @Override
     public void delete(int id) {
-        AddressBookData addressBookData =this.getEmployeeById(id);
+        AddressBookData addressBookData =this.getById(id);
         addressbookRepository.delete(addressBookData);
+    }
+
+    @Override
+    public List<AddressBookData> getAllData() {
+        return addressbookRepository.findAll();
+    }
+
+    @Override
+    public AddressBookData getdataByToken(String token) {
+        int id= jwtToken.decodeToken(token);
+        return addressbookRepository.findById(id).orElseThrow(() -> new CustomException("Employee Not found :- "+id));
     }
 
 
